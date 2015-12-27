a8629cf08b327e9539873497556378343bb6f913
Michael Miles <boat081062@gmail.com>
2015-12-27 16:17:29 -0600

    Started the progrm flow portion starting with If statements.

---
+++ CSharpCourseTemplate6/CSharpCourseTemplate6/Program.cs
@@ -0,0 +1,27 @@
+﻿//Usings allow us to access external features
+using System;
+
+namespace CSharpCourseTemplate6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt for user input
+            Console.WriteLine("Enter String");
+            string s = Console.ReadLine();
+
+            //If conditional statement (== is for comparison, = is for assignment
+            if (s == "")
+            {
+                Console.WriteLine("Please enter a string");
+            }
+            else
+            {
+                Console.WriteLine("Your string is {0} characters long.", s.Length);
+                Console.WriteLine("Could have entered {0} characters more.",
+                    Console.WindowWidth - s.Length);
+            }
+        }
+    }
+}

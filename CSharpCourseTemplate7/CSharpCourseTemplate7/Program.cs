6885f2167470e35c94fa901f15e27c66dfcecdc1
Michael Miles <boat081062@gmail.com>
2015-12-28 05:05:52 -0600

    Lecture 16: While Loops - CSharpCourseTemplate7

---
+++ CSharpCourseTemplate7/CSharpCourseTemplate7/Program.cs
@@ -0,0 +1,33 @@
+﻿//Usings allow us to access external features
+using System;
+
+namespace CSharpCourseTemplate7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How many strings
+            Console.WriteLine("How many turns?");
+            int howMany = int.Parse(Console.ReadLine());
+
+            //Loop counter
+            int loopCounter = 1;
+
+            //Create an empty string
+            string s = "";
+
+            while (loopCounter <= howMany)
+            {
+                //Store strings
+                s += Console.ReadLine();
+
+                //Increase loopCounter by 1
+                loopCounter += 1;
+            }
+            //Print information about strings entered
+            Console.WriteLine("You have entered: {0} characters.", s.Length);
+            Console.WriteLine("Average length of string is: {0} characters.", (double)s.Length / howMany);
+        }
+    }
+}
---
+++ CSharpCourseTemplate8/CSharpCourseTemplate8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Usings allow us to access external features
+using System;
+
+namespace CSharpCourseTemplate8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

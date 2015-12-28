690d2ca2e9b30bb13a540612a79b660b91e20cc9
Michael Miles <boat081062@gmail.com>
2015-12-28 05:51:58 -0600

    Multiple choice decisions with 'If, else if and else - Lecture 17

--- CSharpCourseTemplate8/CSharpCourseTemplate8/Program.cs
+++ CSharpCourseTemplate8/CSharpCourseTemplate8/Program.cs
@@ -7,7 +7,30 @@ namespace CSharpCourseTemplate8
     {
         static void Main(string[] args)
         {
+            //Input from user and convert to decimal format from string
+            Console.WriteLine("Enter Price:");
+            decimal price = decimal.Parse(Console.ReadLine());  //Get Price from User
 
+            //Get rate to calculate increase or decrease in price
+            Console.WriteLine("Enter Rate:");
+            decimal rate = decimal.Parse(Console.ReadLine());   //Get Rate from user
+
+            //Give entry instructions
+            Console.WriteLine("i = Increase, d = Decrease");
+            char choice = (char)Console.Read();
+
+            if (choice== 'i')
+            {
+                Console.WriteLine("The new price is: {0}", (price += price * rate));
+            }
+            else if(choice== 'd')
+            {
+                Console.WriteLine("The new price is: {0}", (price -= price * rate));
+            }
+            else
+            {
+                Console.WriteLine("Your choice does not match list of choices");
+            }
         }
     }
 }

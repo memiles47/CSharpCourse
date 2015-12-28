2f8482eaea8541542d601896abd1af77e5c81776
Michael Miles <boat081062@gmail.com>
2015-12-28 06:28:29 -0600

    Basics of the for loop - Lecture 18

---
+++ CSharpCourseTemplate9/CSharpCourseTemplate9/Program.cs
@@ -0,0 +1,29 @@
+﻿//Usings allow us to access external features
+using System;
+
+namespace CSharpCourseTemplate9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for input
+            Console.WriteLine("Enter String");
+            string s = Console.ReadLine();      //Read input
+            string c = "";                      //Empty string used to accum. parts of s
+
+            //Strings are indexed, for example
+            //The string 'hello', 'h' is a index 0, 'e' is it index 1...'o' is at index 4
+            //String with length of n, means indexes start at 0 until n-1
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //Look at s and grab part of it
+                //part starts at index i and is one character long
+                //Example 'hello'.substring(3,1) would equal 'l' (the second 'l')
+                c += s.Substring(i, 1);
+                Console.WriteLine(c);
+            }
+        }
+    }
+}

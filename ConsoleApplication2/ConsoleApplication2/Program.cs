6aad65c73a17295cc84888a86efa0e18c1784c57
Michael Miles <boat081062@gmail.com>
2016-02-02 14:44:24 -0600

    Complete prior to running

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -15,5 +15,37 @@ namespace Entity
             //Sets the instance variable for the current object
             this.hireDate = dt;
         }
+
+        public DateTime GetHireDate()
+        {
+            //Get us the hire date.
+            //Secure because it only gets the hire date with no ability to change it
+            return this.hireDate;
+        }
+
+        static void Main()
+        {
+            Employee[] employees = new Employee[2];
+
+            //Input by data entry person
+            DateTime hireDate;
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                //Next two line gets hire date
+                Console.Write("Enter Employee # " + i + "Hire Date");
+                hireDate = DateTime.Parse(Console.ReadLine());
+                Console.Clear();
+
+                //Makes new employee with hire date
+                employees[i] = new Employee(hireDate);
+            }
+
+            foreach (Employee emp in employees)
+            {
+                //Displays the hire date for each employee in our array
+                Console.WriteLine("Hired on: " + emp.GetHireDate());
+            }
+        }
     }
 }

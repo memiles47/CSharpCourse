0007c658342abaf0baebb59d984e791594760f09
Michael Miles <boat081062@gmail.com>
2016-02-07 11:13:19 -0600

    Created the challenge for Lecture 42

--- CSharpCourseLecture42/CSharpCourseLecture42/Program.cs
+++ CSharpCourseLecture42/CSharpCourseLecture42/Program.cs
@@ -9,6 +9,16 @@ class DynamicCalc
         return x + y;
     }
 
+    public dynamic Subtract(dynamic x, dynamic y)
+    {
+        return x - y;
+    }
+
+    public dynamic Multiply(dynamic x, dynamic y)
+    {
+        return x * y;
+    }
+
     public dynamic Divide(dynamic x, dynamic y)
     {
         //Ternary operator
@@ -22,7 +32,50 @@ class Program
     {
         DynamicCalc myCalc = new DynamicCalc();
 
-        Console.WriteLine("Sum is: " + myCalc.Add(4,5));
-        Console.WriteLine("Quotient is: " + myCalc.Divide(4,0));
+        Console.Write("Please enter Value 1:> ");
+        double valueOne = double.Parse(Console.ReadLine());
+        Console.Write("Please enter Value 2:> ");
+        double valueTwo = double.Parse(Console.ReadLine());
+
+        string function = SelectFunction();
+
+        switch (function)
+        {
+            case "A":
+                Console.Clear();
+                Console.Write("You have elected to Add and " + "\nthe sum is: ");
+                Console.WriteLine(myCalc.Add(valueOne, valueTwo));
+                break;
+
+            case "S":
+                Console.Clear();
+                Console.Write("You have elected to Subtract and " + "\nthe difference is: ");
+                Console.WriteLine(myCalc.Subtract(valueOne, valueTwo));
+                break;
+
+            case "M":
+                Console.Clear();
+                Console.Write("You have elected to Multiply and " + "\nthe product is: ");
+                Console.WriteLine(myCalc.Multiply(valueOne, valueTwo));
+                break;
+
+            case "D":
+                Console.Clear();
+                Console.Write("You have elected to Divide and " + "\nthe quotient is: ");
+                Console.WriteLine(myCalc.Divide(valueOne, valueTwo));
+                break;
+
+            case "Q":
+                Console.Clear();
+                Console.WriteLine("You have elected to Quit - Bye!");
+                break;
+        }
+    }
+
+    static string SelectFunction()
+    {
+        Console.WriteLine("Please select a function or quit by selecting a letter");
+        Console.Write("<A>dd, <S>ubtract, <M>ultiply, <D>ivide or <Q>uit:>");
+        return Console.ReadLine().ToUpper();
     }
 }

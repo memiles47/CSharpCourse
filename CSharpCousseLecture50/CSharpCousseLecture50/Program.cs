7e53c1e2d618468e887cd60207bbab65bf09581c
Michael Miles <boat081062@gmail.com>
2016-02-11 03:29:53 -0600

    Created more shapes inheariting from the ThreeDShap Abstract class and created multiple volume solutions with the abstract GetVolume() method. Still trying to understand what is good about this code.

--- CSharpCousseLecture50/CSharpCousseLecture50/Program.cs
+++ CSharpCousseLecture50/CSharpCousseLecture50/Program.cs
@@ -44,11 +44,84 @@ class Cube : ThreeDShape
     }
 }
 
+class Cone : ThreeDShape
+{
+    //Height of cone
+    private double coneHeight;
+
+    //Radius of cone base
+    private double coneBaseRadius;
+
+    public Cone(double height, double baseRadius)
+    {
+        //Set instance variables inside constructor
+        this.coneHeight = height;
+        this.coneBaseRadius = baseRadius;
+    }
+
+    public override double GetVolume()
+    {
+        //Cone volume equals pi * r^2 * (h / 3)
+        return Math.PI * Math.Pow(coneBaseRadius, 2) * (coneHeight / 3);
+    }
+}
+
+class Cylinder : ThreeDShape
+{
+    //Height of cylinder
+    private double cylinderHeight;
+
+    //Radius of cylinder base
+    private double cylinderBaseRadius;
+
+    public Cylinder(double height, double baseRadius)
+    {
+        //Set instance variables inside constructor
+        this.cylinderHeight = height;
+        this.cylinderBaseRadius = baseRadius;
+    }
+
+    public override double GetVolume()
+    {
+        //Cylinder volume equals pi * r^2 * h
+        return Math.PI * Math.Pow(cylinderBaseRadius, 2) * cylinderHeight;
+    }
+}
+
+class Pyramid : ThreeDShape
+{
+    //Height of Pyramid
+    private double pyramidHeight;
+
+    //Length of pyramid base
+    private double lengthPyramidBase;
+
+    //Width of pyramid base
+    private double widthPyramidBase;
+
+    public Pyramid(double height, double length, double width)
+    {
+        //Set instance variables inside constructor
+        this.pyramidHeight = height;
+        this.lengthPyramidBase = length;
+        this.widthPyramidBase = width;
+    }
+
+    public override double GetVolume()
+    {
+        //Pyramid volume equals l * w * (h / 3)
+        return lengthPyramidBase * widthPyramidBase * (pyramidHeight / 3);
+    }
+}
+
 class UseThreeDShape
 {
     static void Main()
     {
         Console.WriteLine("Volume of Sphere: " + Math.Round(new Sphere(5).GetVolume(),2));
         Console.WriteLine("Volume of Cube: " + new Cube(5).GetVolume());
+        Console.WriteLine("Volume of Cone: " + Math.Round(new Cone(10,2.5).GetVolume(),2));
+        Console.WriteLine("Volume of Cylinder: " + Math.Round(new Cylinder(10, 2.5).GetVolume(),2));
+        Console.WriteLine("Volume of Pyramid: " + Math.Round(new Pyramid(5, 5, 10).GetVolume(),2));
     }
 }

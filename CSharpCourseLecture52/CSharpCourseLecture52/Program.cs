51eaf37f8b101e483f5aaf7c50aa2247fb2fe0cb
Michael Miles <boat081062@gmail.com>
2016-02-13 17:43:39 -0600

    Getting ready to work on Lecture 52

---
+++ CSharpCourseLecture52/CSharpCourseLecture52/Program.cs
@@ -0,0 +1,16 @@
+﻿//This time and from now on I will be leaving all the using statements in
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourseLecture52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

f7c369e0b4ee60ab7a5bcc3c23f7a79ac54676d4
Michael Miles <boat081062@gmail.com>
2016-02-15 05:28:36 -0600

    More Virtual methods and also polymorphism - There is a lot of learning I need to do.

--- CSharpCourseLecture52/CSharpCourseLecture52/Program.cs
+++ CSharpCourseLecture52/CSharpCourseLecture52/Program.cs
@@ -1,16 +1,65 @@
 ﻿//This time and from now on I will be leaving all the using statements in
+//Name space for lists is in System.Collections.Generic
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpCourseLecture52
+class ComputingDevice
 {
-    class Program
+    //Create instance variables for our class
+    private string name;
+
+    //Create a constructor for our class
+    public ComputingDevice(string nam)
     {
-        static void Main(string[] args)
+        //Set an instance variable
+        this.name = nam;
+    }
+
+    //Create virtual function
+    public virtual void AccetpInput()
+    {
+        Console.WriteLine("Configuring common tasks...");
+    }
+}
+
+//Create new class(s) inheriting from ComputingDevice class
+class Laptop : ComputingDevice
+{
+    public Laptop(string name) : base(name) { }
+    public override void AccetpInput()
+    {
+        base.AccetpInput();
+        Console.WriteLine("Configuring LapTop...");
+    }
+}
+
+class Tablet : ComputingDevice
+{
+    public Tablet(string name) : base(name) { }
+    public override void AccetpInput()
+    {
+        base.AccetpInput();
+        Console.WriteLine("Configuring Tablet...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<ComputingDevice> myList = new List<ComputingDevice>();
+
+        //Storing different kinds of objects in a base typed list
+        myList.Add(new Tablet("My Surface Pro 3"));
+        myList.Add(new Laptop("My Surface Book"));
+
+        //The fact that method calls are correctly resolved at runtime
+        foreach (ComputingDevice cd in myList)
         {
+            cd.AccetpInput();
         }
     }
 }

925828ea574375ebb87d9af67444789dec58bb34
Michael E Miles <michael@miles-me.net>
2016-02-20 14:30:38 -0600

    Lecture 71: Assignment - How to pass arguments to Main...

---
+++ CSharpCourseLecture71/CSharpCourseLecture71/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Lecture 71: Conditional Functions
+*/
+
+class Trace
+{
+    #region
+    [Conditional("DEBUG")]
+    public static void Message(string traceMessage)
+    {
+        Console.WriteLine("[TRACE] - " + traceMessage);
+    }
+    #endregion
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Trace.Message("Main Is Starting");
+
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments have been passed into program");
+        }
+        else
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg[{i}] is [{args[i]}]");
+            }
+        }
+
+        Trace.Message("Main Is Ending");
+    }
+}

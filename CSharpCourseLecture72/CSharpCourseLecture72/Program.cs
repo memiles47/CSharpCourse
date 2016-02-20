82d875f39d340bac809f61c11160f287be2815a1
Michael E Miles <michael@miles-me.net>
2016-02-20 17:58:35 -0600

    Lecture 72: Another basic disk read/write with nullable data types

---
+++ CSharpCourseLecture72/CSharpCourseLecture72/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+/*
+Lecture 72: Nullable Data Type and
+Nammed Parametrs
+*/
+
+class Author
+{
+    //Instance variables for author
+    string name;
+    decimal? salary;
+    bool? published;
+
+    public Author(string fullName, decimal? sal, bool? pub)
+    {
+        this.name = fullName;
+        this.salary = sal;
+        this.published = pub;
+    }
+
+    //Save to disk with function or method
+    public void WriteAuthorToDisk(string path)
+    {
+        using(StreamWriter writer = File.CreateText(@path))
+        {
+            writer.WriteLine(this.name);
+            writer.WriteLine(this.salary);
+            writer.WriteLine(this.published);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Author aut = new Author(pub: null, sal: 54500M, fullName: "Bubba Jones");
+
+        //Prompt user for path
+        Console.Write("Enter Path:>");
+
+        //Read path in
+        string s = Console.ReadLine();
+
+        //Write author to disk
+        aut.WriteAuthorToDisk(s);
+    }
+}

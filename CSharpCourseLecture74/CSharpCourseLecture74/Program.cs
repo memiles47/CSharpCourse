c9ef38721650cb5c5afe5452bb53c26614726e35
Michael E Miles <michael@miles-me.net>
2016-02-21 13:28:24 -0600

    Lecture 74: Awsome qualified names lecture

---
+++ CSharpCourseLecture74/CSharpCourseLecture74/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ComputingServices.ArithmeticServices;
+using ComputingServices.FinanceServices;
+
+/*
+Lecture 74: Fully Qualified Names
+*/
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Sum of 4 and 5 is {BasicMath.Add(4, 5)}");
+        Console.WriteLine($"This interest amount is {FinanceMath.FindInterest(0.05, 56788)}");
+    }
+}
---
+++ CSharpCourseLecture74/ComputingServices/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace ComputingServices
+{
+    namespace ArithmeticServices
+    {
+        public class BasicMath
+        {
+            public static double Add(double x, double y)
+            {
+                return x + y;
+            }
+        }
+    }
+
+    namespace FinanceServices
+    {
+        public class FinanceMath
+        {
+            public static double FindInterest(double rate, double amount)
+            {
+                return rate * amount;
+            }
+        }
+    }
+}

27e3f533c3e05f715f0f45b85f33977f5c543719
Michael E Miles <michael@miles-me.net>
2016-02-21 09:59:54 -0600

    Lecture 73: Good lesson on Dynamic Link Library

---
+++ CompServices/CompServices/Class1.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+Lecture 73: Create a Dynamic Link
+Library and Reference it
+*/
+
+namespace CompServices
+{
+    public class Caluclator
+    {
+        public dynamic Add(dynamic x, dynamic y)
+        {
+            return x + y;
+        }
+
+        public dynamic Divide(dynamic x, dynamic y)
+        {
+            return y != 0 ? x / y : "Error: Divide by Zero";
+        }
+    }
+}

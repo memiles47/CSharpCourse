663c97f49d7a708ca38b8ee339a9df457fdc720a
Michael E Miles <michael@miles-me.net>
2016-02-23 11:57:45 -0600

    Lecture 78: Create List with undefined list use ForEach

---
+++ CSharpCourseLecture78/CSharpCourseLecture78/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+Lecture 78: List<T> With Predefined
+Types and ForEach
+*/
+
+class Program
+{
+    static void Case(string p)
+    {
+        Console.WriteLine(p.ToLower());
+    }
+
+    static void Main()
+    {
+        //Creates list of strings with no defined size
+        List<string> myWords = new List<string>();
+
+        string s = Console.ReadLine();
+        while(s.ToLower() != "exit")
+        {
+            myWords.Add(s);
+            s = Console.ReadLine();
+        }
+
+        myWords.ForEach(Case);
+        myWords.Reverse();
+        myWords.ForEach(Case);
+    }
+}

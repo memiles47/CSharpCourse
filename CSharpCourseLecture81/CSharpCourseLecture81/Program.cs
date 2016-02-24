d8e97224248761ea0e1f860b0d21e381637c91be
Michael Miles <boat081062@gmail.com>
2016-02-24 04:21:52 -0600

    Lecture 81: Lamda expressions and statements

---
+++ CSharpCourseLecture81/CSharpCourseLecture81/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Lecture 81: Lamda "=>" Expressions and
+Lamda statements
+*/
+
+delegate double Multiply(double x, double y);
+delegate double CompareFirstTwo(double x, double y);
+
+
+class Program
+{
+    static void Main()
+    {
+        Multiply mult = (x, y) => (x * y);
+        Console.WriteLine($"The product of 4.5 and 3.8 is {mult(4.5,3.8)}");
+
+        CompareFirstTwo cft = (xin, yin) =>
+        {
+            double x = xin;
+            double y = yin;
+
+            return x > y ? x : y;
+        };
+
+        Console.WriteLine(CompareThird(cft, 10));
+    }
+
+    private static double CompareThird(CompareFirstTwo cf, double x)
+    {
+        return x > cf(4, 5) ? x : cf(4, 5);
+    }
+}

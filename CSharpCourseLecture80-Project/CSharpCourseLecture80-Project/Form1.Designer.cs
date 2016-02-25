7a8a11851fb931bf9376b35436fdd772b029bcb6
Michael Miles <boat081062@gmail.com>
2016-02-25 06:34:28 -0600

    Lecture 80 Project: Form set up.

--- CSharpCourseLecture80-Project/CSharpCourseLecture80-Project/Form1.Designer.cs
+++ CSharpCourseLecture80-Project/CSharpCourseLecture80-Project/Form1.Designer.cs
@@ -32,13 +32,20 @@
             this.tb_key = new System.Windows.Forms.TextBox();
             this.lbl_value = new System.Windows.Forms.Label();
             this.tb_value = new System.Windows.Forms.TextBox();
+            this.btn_addToDictionary = new System.Windows.Forms.Button();
+            this.btn_saveToDisk = new System.Windows.Forms.Button();
+            this.btn_openInNotepad = new System.Windows.Forms.Button();
+            this.lbl_enterPath = new System.Windows.Forms.Label();
+            this.tb_path = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.lbl_saveToDisk = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_key
             // 
             this.lbl_key.AutoSize = true;
             this.lbl_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_key.Location = new System.Drawing.Point(12, 18);
+            this.lbl_key.Location = new System.Drawing.Point(331, 53);
             this.lbl_key.Name = "lbl_key";
             this.lbl_key.Size = new System.Drawing.Size(54, 29);
             this.lbl_key.TabIndex = 0;
@@ -47,7 +54,7 @@
             // tb_key
             // 
             this.tb_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_key.Location = new System.Drawing.Point(92, 12);
+            this.tb_key.Location = new System.Drawing.Point(421, 47);
             this.tb_key.Name = "tb_key";
             this.tb_key.Size = new System.Drawing.Size(150, 35);
             this.tb_key.TabIndex = 1;
@@ -56,7 +63,7 @@
             // 
             this.lbl_value.AutoSize = true;
             this.lbl_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_value.Location = new System.Drawing.Point(12, 71);
+            this.lbl_value.Location = new System.Drawing.Point(331, 106);
             this.lbl_value.Name = "lbl_value";
             this.lbl_value.Size = new System.Drawing.Size(74, 29);
             this.lbl_value.TabIndex = 2;
@@ -65,16 +72,91 @@
             // tb_value
             // 
             this.tb_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_value.Location = new System.Drawing.Point(92, 65);
+            this.tb_value.Location = new System.Drawing.Point(421, 100);
             this.tb_value.Name = "tb_value";
             this.tb_value.Size = new System.Drawing.Size(150, 35);
             this.tb_value.TabIndex = 3;
+            // 
+            // btn_addToDictionary
+            // 
+            this.btn_addToDictionary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_addToDictionary.Location = new System.Drawing.Point(49, 166);
+            this.btn_addToDictionary.Name = "btn_addToDictionary";
+            this.btn_addToDictionary.Size = new System.Drawing.Size(791, 40);
+            this.btn_addToDictionary.TabIndex = 4;
+            this.btn_addToDictionary.Text = "Add To Dictionary";
+            this.btn_addToDictionary.UseVisualStyleBackColor = true;
+            // 
+            // btn_saveToDisk
+            // 
+            this.btn_saveToDisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_saveToDisk.Location = new System.Drawing.Point(49, 228);
+            this.btn_saveToDisk.Name = "btn_saveToDisk";
+            this.btn_saveToDisk.Size = new System.Drawing.Size(791, 40);
+            this.btn_saveToDisk.TabIndex = 5;
+            this.btn_saveToDisk.Text = "Save To Disk";
+            this.btn_saveToDisk.UseVisualStyleBackColor = true;
+            // 
+            // btn_openInNotepad
+            // 
+            this.btn_openInNotepad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_openInNotepad.Location = new System.Drawing.Point(49, 290);
+            this.btn_openInNotepad.Name = "btn_openInNotepad";
+            this.btn_openInNotepad.Size = new System.Drawing.Size(791, 40);
+            this.btn_openInNotepad.TabIndex = 6;
+            this.btn_openInNotepad.Text = "Open in Notepad";
+            this.btn_openInNotepad.UseVisualStyleBackColor = true;
+            // 
+            // lbl_enterPath
+            // 
+            this.lbl_enterPath.AutoSize = true;
+            this.lbl_enterPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_enterPath.Location = new System.Drawing.Point(44, 359);
+            this.lbl_enterPath.Name = "lbl_enterPath";
+            this.lbl_enterPath.Size = new System.Drawing.Size(124, 29);
+            this.lbl_enterPath.TabIndex = 7;
+            this.lbl_enterPath.Text = "Enter Path";
+            // 
+            // tb_path
+            // 
+            this.tb_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_path.Location = new System.Drawing.Point(174, 353);
+            this.tb_path.Name = "tb_path";
+            this.tb_path.Size = new System.Drawing.Size(550, 35);
+            this.tb_path.TabIndex = 8;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_browse.Location = new System.Drawing.Point(730, 350);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(110, 40);
+            this.btn_browse.TabIndex = 9;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            // 
+            // lbl_saveToDisk
+            // 
+            this.lbl_saveToDisk.AutoSize = true;
+            this.lbl_saveToDisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_saveToDisk.Location = new System.Drawing.Point(49, 494);
+            this.lbl_saveToDisk.Name = "lbl_saveToDisk";
+            this.lbl_saveToDisk.Size = new System.Drawing.Size(176, 29);
+            this.lbl_saveToDisk.TabIndex = 10;
+            this.lbl_saveToDisk.Text = "Saved To Disk:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 491);
+            this.ClientSize = new System.Drawing.Size(902, 591);
+            this.Controls.Add(this.lbl_saveToDisk);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.tb_path);
+            this.Controls.Add(this.lbl_enterPath);
+            this.Controls.Add(this.btn_openInNotepad);
+            this.Controls.Add(this.btn_saveToDisk);
+            this.Controls.Add(this.btn_addToDictionary);
             this.Controls.Add(this.tb_value);
             this.Controls.Add(this.lbl_value);
             this.Controls.Add(this.tb_key);
@@ -92,6 +174,13 @@
         private System.Windows.Forms.TextBox tb_key;
         private System.Windows.Forms.Label lbl_value;
         private System.Windows.Forms.TextBox tb_value;
+        private System.Windows.Forms.Button btn_addToDictionary;
+        private System.Windows.Forms.Button btn_saveToDisk;
+        private System.Windows.Forms.Button btn_openInNotepad;
+        private System.Windows.Forms.Label lbl_enterPath;
+        private System.Windows.Forms.TextBox tb_path;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Label lbl_saveToDisk;
     }
 }
 

502fb8d2842dd24f5aee919ad9a44ea523fb803d
Michael Miles <boat081062@gmail.com>
2016-02-25 06:16:37 -0600

    Lecture 80 Project: Starting to build the form

---
+++ CSharpCourseLecture80-Project/CSharpCourseLecture80-Project/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CSharpCourseLecture80_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_key = new System.Windows.Forms.Label();
+            this.tb_key = new System.Windows.Forms.TextBox();
+            this.lbl_value = new System.Windows.Forms.Label();
+            this.tb_value = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_key
+            // 
+            this.lbl_key.AutoSize = true;
+            this.lbl_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_key.Location = new System.Drawing.Point(12, 18);
+            this.lbl_key.Name = "lbl_key";
+            this.lbl_key.Size = new System.Drawing.Size(54, 29);
+            this.lbl_key.TabIndex = 0;
+            this.lbl_key.Text = "Key";
+            // 
+            // tb_key
+            // 
+            this.tb_key.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_key.Location = new System.Drawing.Point(92, 12);
+            this.tb_key.Name = "tb_key";
+            this.tb_key.Size = new System.Drawing.Size(150, 35);
+            this.tb_key.TabIndex = 1;
+            // 
+            // lbl_value
+            // 
+            this.lbl_value.AutoSize = true;
+            this.lbl_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_value.Location = new System.Drawing.Point(12, 71);
+            this.lbl_value.Name = "lbl_value";
+            this.lbl_value.Size = new System.Drawing.Size(74, 29);
+            this.lbl_value.TabIndex = 2;
+            this.lbl_value.Text = "Value";
+            // 
+            // tb_value
+            // 
+            this.tb_value.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_value.Location = new System.Drawing.Point(92, 65);
+            this.tb_value.Name = "tb_value";
+            this.tb_value.Size = new System.Drawing.Size(150, 35);
+            this.tb_value.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 491);
+            this.Controls.Add(this.tb_value);
+            this.Controls.Add(this.lbl_value);
+            this.Controls.Add(this.tb_key);
+            this.Controls.Add(this.lbl_key);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_key;
+        private System.Windows.Forms.TextBox tb_key;
+        private System.Windows.Forms.Label lbl_value;
+        private System.Windows.Forms.TextBox tb_value;
+    }
+}
+

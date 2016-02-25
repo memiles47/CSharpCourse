e805d5c12002886d04c8cf0546323ddd85e2ec28
Michael E Miles <michael@miles-me.net>
2016-02-25 17:26:35 -0600

    Minor revisions to comments

---
+++ CSharpCourseLecture73/CSharpCourseLecture73/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CompServices;
+
+/*
+Lecture 73: Create a Dynamic Link
+Librry and Reference it
+*/
+
+namespace CSharpCourseLecture73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new Calculator object
+            Caluclator calc = new Caluclator();
+
+            //Use methods within the Calculator object
+            Console.WriteLine(calc.Add(4, 5));
+            Console.WriteLine(calc.Divide(10, 5));
+            Console.WriteLine(calc.Divide(4, 0));
+        }
+    }
+}

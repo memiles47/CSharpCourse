22000d59aa32c0cc8a117f36c577b7cf2f402a51
Michael E Miles <michael@miles-me.net>
2016-02-28 15:51:56 -0600

    Lecture 90: Creating WPF applications

---
+++ CSharpCourseLecture90/CSharpCourseLecture90/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.IO;
+
+namespace CSharpCourseLecture90
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = Directory.GetFiles(tb_filesBox.Text);
+            foreach (string f in files)
+            {
+                rtb_displayBox.AppendText($"{f.ToString()} \n");
+            }
+        }
+    }
+}

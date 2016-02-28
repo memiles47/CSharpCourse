7a8fd6d91e4818319b06a56cbee4cfb024d299f3
Michael E Miles <michael@miles-me.net>
2016-02-28 10:05:12 -0600

    Lecture 86: Simple query written to pull data from and xml file

---
+++ CSharpCourseLecture86a/CSharpCourseLecture86a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+/*
+Lecture 86: Use LINQ with XML
+*/
+
+namespace CSharpCourseLecture86a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement userFile = XElement.Load(@"F:\Udemy\VisualStudio\CSharpCourse\CSharpCourseLecture86a\CSharpCourseLecture86a\users.xml");
+
+            IEnumerable<string> deskTopUser = 
+
+                //An impeded query, look inside the document and get each user
+                from eachuser in userFile.Descendants("user")
+
+                //Get full name of each user
+                select (string)eachuser.Attribute("fullname");
+
+            foreach (string name in deskTopUser)
+            {
+                Console.WriteLine($"The name is {name}");
+            }
+        }
+    }
+}

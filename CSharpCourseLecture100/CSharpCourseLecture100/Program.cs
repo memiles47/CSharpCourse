761bb8a351e57c9f8913ecf052d46a9fadc75db8
Michael Miles <boat081062@gmail.com>
2016-03-01 06:26:56 -0600

    Lecture 100: Serialization - converting text to a binary files. (100 lessons completeed, 17 to go!)

---
+++ CSharpCourseLecture100/CSharpCourseLecture100/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/*
+Lecture 100: Serialization Example
+*/
+
+[Serializable()]
+
+class Person
+{
+    public string fName;
+
+    public Person(string first)
+    {
+        this.fName = first;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person per = new Person("James");
+
+        //Takes care of the serialization process
+        BinaryFormatter binFormatter = new BinaryFormatter();
+
+        //
+        using (Stream writer = File.Create(@"c:\users\Michael\data\person.bin"))
+        {
+            binFormatter.Serialize(writer, per);
+        }
+
+        using (Stream reader = File.OpenRead(@"c:\users\Michael\data\person.bin"))
+        {
+            Person per2 = (Person)binFormatter.Deserialize(reader);
+            Console.WriteLine($"First name is {per2.fName}");
+        }
+    }
+}

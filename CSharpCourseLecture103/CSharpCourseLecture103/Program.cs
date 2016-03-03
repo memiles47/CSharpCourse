2a9f2781e95b730730c4ef98bc9d9889e3e09741
Michael Miles <boat081062@gmail.com>
2016-03-03 14:37:24 -0600

    Add some excluede change documents.

---
+++ CSharpCourseLecture103/CSharpCourseLecture103/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourseLecture103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+    }
+}
